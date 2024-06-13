86b6f1112910c1393c67635ee0217b3ce2d57a0e
Brad Barrows <bradley.barrows@pluto.tv>
2024-06-13 14:34:57 -0700

    Have 2 balls that collide and a line that shows diretion going following mouse, rotationg after collision

---
+++ CircleStaticBody2D.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+@export var radius: float = 20.0
+
+func _draw():
+	draw_circle(Vector2.ZERO, radius, Color.WHITE)	
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#var ccol = $CircleCollisionShape2D
+	#ccol.transform.scale
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

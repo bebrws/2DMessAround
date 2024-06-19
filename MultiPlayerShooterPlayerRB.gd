4f9995cb0f2158b7c611573673a8b0b58e9c453b
Brad Barrows <bradley.barrows@pluto.tv>
2024-06-19 06:45:50 -0700

    player movemnt good for now

--- MultiPlayerShooterPlayerRB.gd
+++ MultiPlayerShooterPlayerRB.gd
@@ -208,7 +208,7 @@ func _process(delta: float) -> void:
 			facing_right = true
 			sprite.set_flip_h(false)
 			#var f = force_to_gravity.orthogonal().normalized() * delta * 100000.0
-			var f = cn.global_position.direction_to(ln.global_position) * delta * 100000.0
+			var f = cn.global_position.direction_to(rn.global_position) * delta * 100000.0
 			print("right f ", f)
 			print("rot ", sprite.rotation_degrees)
 			self.apply_central_force(f)
@@ -218,18 +218,18 @@ func _process(delta: float) -> void:
 			facing_right = false
 			sprite.set_flip_h(true)
 			#var f = force_to_gravity.orthogonal().rotated(deg_to_rad(180)).normalized() * delta * 100000.0
-			var f = cn.global_position.direction_to(rn.global_position) * delta * 100000.0
+			var f = cn.global_position.direction_to(ln.global_position) * delta * 100000.0
 			print("left f ", f)
 			print("rot ", sprite.rotation_degrees)
 			self.apply_central_force(f)
 		
 		if Input.is_action_pressed("ui_up"):
-			var f = Vector2.UP * delta * 100000.0
+			var f = cn.global_position.direction_to(hn.global_position) * delta * 100000.0
 			print("up f ", f)
 			self.apply_central_force(f)
 			#self.linear_velocity += (self.global_position.direction_to(get_global_mouse_position()).normalized() * 50.0)
 		if Input.is_action_pressed("ui_down"):
-			var f = Vector2.DOWN * delta * 100000.0
+			var f = cn.global_position.direction_to(fn.global_position) * delta * 100000.0
 			print("down f ", f)
 			self.apply_central_force(f)
 			#self.linear_velocity -= (self.global_position.direction_to(get_global_mouse_position()).normalized() * 50.0)
